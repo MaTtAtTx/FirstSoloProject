c4efb9888a071af35179ef91a17477eab3cfb49b
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-09-28 09:13:21 -0600

    Changed a println to a print to test in java.

--- src/solo/controller/SoloController.java
+++ src/solo/controller/SoloController.java
@@ -5,6 +5,6 @@ public class SoloController
 	public void start()
 	{
 		System.out.println("This is a fun first project to make");
-		System.out.println("hello");
+		System.out.print("hello");
 	}
 }
